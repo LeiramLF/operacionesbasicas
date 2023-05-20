5c2525c06b39d3fcbf3a80d4c15d75bec56d4d14
Leiram ⚡️ <133523510+LeiramLF@users.noreply.github.com>
2023-05-20 01:31:50 -0600

    Add files via upload

---
+++ prog_2.py
@@ -0,0 +1,9 @@
+a= float(input("Inserta un numero:"))# ingresa un valor flotante para la variable a aquí
+b= float(input("Inserta un numero:"))# ingresa un valor flotante para la variable b aquí
+
+print("La suma de ", a," + ",b,"es: " + str(a+b))# muestra el resultado de la suma aquí 
+print("La resta de ",a," - ",b,"es: " + str(a-b))# muestra el resultado de la resta aquí
+print("La multiplicación de ",a," x ",b," es: " + str(a*b))# muestra el resultado de la multiplicación aquí
+print("La division de ",a," / ",b," es: " + str(a/b))# muestra el resultado de la división aquí
+
+print("\n¡Eso es todo, amigos!")
